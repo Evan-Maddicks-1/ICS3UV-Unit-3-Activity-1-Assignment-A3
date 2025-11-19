9d7fc410b608887f5ff0202b42061ca4f2360adf
default <evan.maddicks@stu.ocsb.ca>
2025-11-19 01:11:33 +0000

    code functions as intended

---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+* @author Evan Maddicks
+* @version 1.0.0
+* @date 2025-11-18
+* @fileoverview this program creates a multiplication table from 0 to 12 with a multiple of 9
+*/
+let multiple = 9
+
+console.log("0 x 9 =" + 0 * multiple)
+console.log("1 x 9 =" + 1 * multiple)
+console.log("2 x 9 =" + 2 * multiple)
+console.log("3 x 9 =" + 3 * multiple)
+console.log("4 x 9 =" + 4 * multiple)
+console.log("5 x 9 =" + 5 * multiple)
+console.log("6 x 9 =" + 6 * multiple)
+console.log("7 x 9 =" + 7 * multiple)
+console.log("8 x 9 =" + 8 * multiple)
+console.log("9 x 9 =" + 9 * multiple)
+console.log("10 x 9 =" + 10 * multiple)
+console.log("11 x 9 =" + 11 * multiple)
+console.log("12 x 9 =" + 12 * multiple)
